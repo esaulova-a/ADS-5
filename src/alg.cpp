1a367cffd8e7ef316dcdb09f624e9dbde12b4217
esaulova-a <160412658+esaulova-a@users.noreply.github.com>
2024-05-09 21:26:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,71 @@
 #include <map>
 #include "tstack.h"
 
+TStack<char, 100> stack1;
+TStack<int, 100> stack2;
+
+int findPriority(char sign) {
+  if (sign == '+' || sign == '-') return 1;
+  if (sign == '*' || sign == '/') return 2;
+  return 0;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  std::string postfix;
+  TStack<char, 100> stack;
+  for (char c : inf) {
+    if (isdigit(c)) {
+      postfix = postfix + c + ' ';
+    } else if (c == '(') {
+      stack.add(c);
+    } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+      while (!stack.isEmpty() && findPriority(stack.show()) >= findPriority(c)) {
+        postfix = postfix + stack.show() + ' ';
+        stack.pop();
+      }
+      stack.add(c);
+    } else if (c == ')') {
+      while (!stack.isEmpty() && stack.show() != '(') {
+        postfix = postfix + stack.show() + ' ';
+        stack.pop();
+      }
+      stack.pop();
+    }
+  }
+  while (!stack.isEmpty()) {
+    postfix = postfix + stack.show() + ' ';
+    stack.pop();
+  }
+  if (!postfix.empty()) {
+    postfix.pop_back();
+  }
+  return postfix;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  std::string strNumber = "";
+  for (char c : pref) {
+    if (c != ' ' && c != '+' && c != '-' && c != '*' && c != '/') {
+      strNumber += c;
+    }
+    if (c == ' ') {
+      if (!strNumber.empty()) {
+        int num = std::stoi(strNumber);
+        stack2.add(num);
+        strNumber = "";
+      }
+    }
+    if (c == '+' || c == '-' || c == '*' || c == '/') {
+      int b = stack2.show();
+      stack2.pop();
+      int a = stack2.show();
+      stack2.pop();
+
+      if (c == '+') stack2.add((a + b));
+      else if (c == '-') stack2.add((a - b));  
+      else if (c == '*') stack2.add((a * b));
+      else if (c == '/') stack2.add((a / b));
+    }
+  }
+  return stack2.show();
 }
