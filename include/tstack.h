4f2eef915c9289528d8db1e1cbe319a0d0a2d979
esaulova-a <160412658+esaulova-a@users.noreply.github.com>
2024-05-09 21:33:24 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -6,19 +6,24 @@
 template<typename T, int size>
 class TStack {
  private:
-    T data[Size];
-    int top;
+  T data[100];
+  int top;
 
  public:
   TStack() : top(-1) {}
   void add(T value) {
-    if (!isFull()) data[++top] = value;
-    else throw std::string("Full");
+    if (!isFull())
+      data[++top] = value;
+    else
+      throw std::string("Full");
   }
   T show() const { return data[top]; }
   T pop() {
-    if (isEmpty()) throw std::string("Empty");
-    else return data[top--];
+    if (isEmpty()) {
+      throw std::string("Empty");
+    } else {
+      return data[top--];
+    }
   }
   bool isEmpty() const { return top == -1; }
   bool isFull() const { return top == size - 1; }
