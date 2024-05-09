7b7b9dda65928d9fbece61f0693febfdbcd5a39c
esaulova-a <160412658+esaulova-a@users.noreply.github.com>
2024-05-09 21:17:34 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,41 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[Size];
+    int top;
+
+ public:
+    TStack() : top(0) {}
+
+    void push(const T &item) {
+        if (top <= Size - 1)
+            data[top++] = item;
+        else
+            throw std::string("Full!");
+    }
+
+    T pop() {
+        if (top > 0)
+            return data[--top];
+        else
+            throw std::string("Empty!");
+    }
+
+    bool isEmpty() {
+        return top == 0;
+    }
+
+    T get() {
+        if (!isEmpty())
+            return data[top - 1];
+        else
+            throw std::string("is Empty");
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
